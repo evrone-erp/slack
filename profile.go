a81ec673284dc18c0bfd5aac4b1ae92107452af5
Вершинин Павел Николаевич <pavel.vershinin@evrone.com>
2022-09-20 13:36:58 +0300

    users.profile.set json

--- profile.go
+++ profile.go
@@ -12,21 +12,21 @@ func (api *Client) SetUserProfile(user string, profile *UserProfile) error {
 
 // SetUserProfileContext will set a profile for the provided user with a custom context
 func (api *Client) SetUserProfileContext(ctx context.Context, user string, profile *UserProfile) error {
-	jsonProfile, err := json.Marshal(map[string]interface{}{
+	data := map[string]interface{}{
 		"profile": profile,
-	})
+	}
 
+	if user != "" {
+		data["user"] = user
+	}
+
+	jsonProfile, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
 
-	path := "users.profile.set"
-	if user != "" {
-		path += "?user=" + user
-	}
-
 	response := &userResponseFull{}
-	if err = api.jsonMethod(ctx, path, jsonProfile, response); err != nil {
+	if err = api.jsonMethod(ctx, "users.profile.set", jsonProfile, response); err != nil {
 		return err
 	}
 
